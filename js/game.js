0c5b473fce66763226bbaa195b7a9bc65560b32a
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-02-16 23:12:01 +0000

    spacing fix

--- js/game.js
+++ js/game.js
@@ -1,10 +1,9 @@
 /* global Phaser */
-//* Game scene */
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  // set background color
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
